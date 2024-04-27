6b7f55eadf6d48d79fa57ca1f2e33e699a5f98a3
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-04-27 12:53:39 -0400

    solved phase 3

--- aa04-class-employee-manager/employee.js
+++ aa04-class-employee-manager/employee.js
@@ -4,10 +4,18 @@ class Employee {
         this.salary = salary;
         this.title = title;
         this.manager = manager ?? null;
+
+        // If we have passed in a mgr, then add
+        // employee to that manager upon creation
+        if (this.manager) {
+            manager.addEmployee(this)
+        }
     }
+
+
 }
 
-const leo = new Employee('Leonardo', 90000, 'Ninja');
-console.log(leo)
+// const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+// console.log(leo)
 
 module.exports = Employee;
--- aa04-class-employee-manager/manager.js
+++ aa04-class-employee-manager/manager.js
@@ -8,4 +8,19 @@ class Manager extends Employee {
     addEmployee(employee) {
         this.employees.push(employee)
     }
+
+
 }
+
+
+const splinter = new Manager('Splinter', 100000, 'Sensei');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+console.log('After: ', splinter);
+
+module.exports = Manager
